44fd659b12d17262cf301b9dd829f94127b128a2
KeaMeng <2023484ing@aupp.edu.kh>
2024-08-17 21:17:59 +0700

    All Logic Done, No UI

--- script.js
+++ script.js
@@ -10,40 +10,47 @@ function operation() {
     pad.addEventListener("click", function (event) {
       const value = event.target.value;
       if (event.target.classList.contains("clear")) {
-        currentOperand = "";
+        currentOperand = "0";
         previousOperand = "";
         result = "";
         operator = "";
         display.innerHTML = "0";
-      } else if (event.target.classList.contains("digit")) {
+      } else if (event.target.classList.contains("dot")) {
+        if (currentOperand.includes(".")) return;
         currentOperand += value;
-        display.innerHTML = currentOperand;
-        console.log(`Current ${currentOperand}`);
-        console.log(`Pre ${previousOperand}`);
+      } else if (event.target.classList.contains("digit")) {
+        if (
+          currentOperand.includes(".") &&
+          currentOperand.split(".")[1].length > 0
+        )
+          return;
+        currentOperand += value;
       } else if (event.target.classList.contains("operator")) {
         if (currentOperand == "" && previousOperand == "") return;
         previousOperand = currentOperand;
         currentOperand = "";
         operator = value;
-        console.log(`Current ${currentOperand}`);
-        console.log(`Pre ${previousOperand}`);
+        return;
       } else if (event.target.classList.contains("equal")) {
         if (currentOperand == "" || previousOperand == "") return;
         result = calculate(currentOperand, previousOperand, operator);
+        if (String(result).includes(".")) {
+          result = Number.parseFloat(result).toFixed(1);
+        }
         currentOperand = result;
-        display.innerHTML = currentOperand;
         operator = "";
         previousOperand = "";
-        console.log(`Current ${currentOperand}`);
-        console.log(`Pre ${previousOperand}`);
+      } else if (event.target.classList.contains("backspace")) {
+        currentOperand = currentOperand.slice(0, -1);
       }
+      display.innerHTML = currentOperand;
     });
   });
 }
 
 function calculate(firstValue, secondValue, operator) {
-  let intValue1 = parseInt(firstValue);
-  let intValue2 = parseInt(secondValue);
+  let intValue1 = parseFloat(firstValue);
+  let intValue2 = parseFloat(secondValue);
   if (operator == "+") {
     return intValue1 + intValue2;
   } else if (operator == "-") {
